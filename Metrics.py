6b14dd0f144522b3112715eb8f64384344b563ac
xyuanLin <94221163+xyuanLin@users.noreply.github.com>
2022-06-07 16:32:29 +0800

    Update and rename metrics.py to Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -2,56 +2,35 @@ import numpy as np
 import cv2
 import os
 
-""" 
-混淆矩阵
-P\L     P    N 
-P      TP    FP 
-N      FN    TN 
-"""
-
-
-#  获取颜色字典
-#  labelFolder 标签文件夹,之所以遍历文件夹是因为一张标签可能不包含所有类别颜色
-#  classNum 类别总数(含背景)
 def color_dict(labelFolder, classNum):
     colorDict = []
-    #  获取文件夹内的文件名
     ImageNameList = os.listdir(labelFolder)
     for i in range(len(ImageNameList)):
         ImagePath = labelFolder + "/" + ImageNameList[i]
         img = cv2.imread(ImagePath).astype(np.uint32)
-        #  如果是灰度，转成RGB
         if (len(img.shape) == 2):
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB).astype(np.uint32)
-        #  为了提取唯一值，将RGB转成一个数
         img_new = img[:, :, 0] * 1000000 + img[:, :, 1] * 1000 + img[:, :, 2]
         unique = np.unique(img_new)
-        #  将第i个像素矩阵的唯一值添加到colorDict中
         for j in range(unique.shape[0]):
             colorDict.append(unique[j])
-        #  对目前i个像素矩阵里的唯一值再取唯一值
+
         colorDict = sorted(set(colorDict))
-        #  若唯一值数目等于总类数(包括背景)ClassNum，停止遍历剩余的图像
+
         if (len(colorDict) == classNum):
             break
-    #  存储颜色的BGR字典，用于预测时的渲染结果
     colorDict_BGR = []
     for k in range(len(colorDict)):
-        #  对没有达到九位数字的结果进行左边补零(eg:5,201,111->005,201,111)
         color = str(colorDict[k]).rjust(9, '0')
-        #  前3位B,中3位G,后3位R
         color_BGR = [int(color[0: 3]), int(color[3: 6]), int(color[6: 9])]
         colorDict_BGR.append(color_BGR)
-    #  转为numpy格式
     colorDict_BGR = np.array(colorDict_BGR)
-    #  存储颜色的GRAY字典，用于预处理时的onehot编码
     colorDict_GRAY = colorDict_BGR.reshape((colorDict_BGR.shape[0], 1, colorDict_BGR.shape[1])).astype(np.uint8)
     colorDict_GRAY = cv2.cvtColor(colorDict_GRAY, cv2.COLOR_BGR2GRAY)
     return colorDict_BGR, colorDict_GRAY
 
 
 def ConfusionMatrix(numClass, imgPredict, Label):
-    #  返回混淆矩阵
     mask = (Label >= 0) & (Label < numClass)
     label = numClass * Label[mask] + imgPredict[mask]
     count = np.bincount(label, minlength=numClass ** 2)
@@ -60,20 +39,18 @@ def ConfusionMatrix(numClass, imgPredict, Label):
 
 
 def OverallAccuracy(confusionMatrix):
-    #  返回所有类的整体像素精度OA
     # acc = (TP + TN) / (TP + TN + FP + TN)
     OA = np.diag(confusionMatrix).sum() / confusionMatrix.sum()
     return OA
 
 
 def Precision(confusionMatrix):
-    #  返回所有类别的精确率precision
     precision = np.diag(confusionMatrix) / confusionMatrix.sum(axis=0)
     return precision
 
 
 def Recall(confusionMatrix):
-    #  返回所有类别的召回率recall
+
     recall = np.diag(confusionMatrix) / confusionMatrix.sum(axis=1)
     return recall
 
@@ -86,7 +63,7 @@ def F1Score(confusionMatrix):
 
 
 def IntersectionOverUnion(confusionMatrix):
-    #  返回交并比IoU
+
     intersection = np.diag(confusionMatrix)
     union = np.sum(confusionMatrix, axis=1) + np.sum(confusionMatrix, axis=0) - np.diag(confusionMatrix)
     IoU = intersection / union
@@ -94,7 +71,7 @@ def IntersectionOverUnion(confusionMatrix):
 
 
 def MeanIntersectionOverUnion(confusionMatrix):
-    #  返回平均交并比mIoU
+
     intersection = np.diag(confusionMatrix)
     union = np.sum(confusionMatrix, axis=1) + np.sum(confusionMatrix, axis=0) - np.diag(confusionMatrix)
     IoU = intersection / union
@@ -103,7 +80,6 @@ def MeanIntersectionOverUnion(confusionMatrix):
 
 
 def Frequency_Weighted_Intersection_over_Union(confusionMatrix):
-    #  返回频权交并比FWIoU
     freq = np.sum(confusionMatrix, axis=1) / np.sum(confusionMatrix)
     iu = np.diag(confusionMatrix) / (
             np.sum(confusionMatrix, axis=1) +
